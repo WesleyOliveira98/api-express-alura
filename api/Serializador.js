c72cd3723719a23e170e847f47cfda6138a08a8e
Wesley <wesleygabrieldeoliveira@gmail.com>
2022-02-20 19:01:52 -0300

    Adicionando o tipo XML como tipo aceito por nossa API; Criação dos métodos XML usando jsontoxml para que as respostas retornadas sejam em XML

--- api/Serializador.js
+++ api/Serializador.js
@@ -1,14 +1,28 @@
 const ValorNaoSuportado = require('./erros/ValorNaoSuportado')
+const jsontoxml = require('jsontoxml')
 
 class Serializador {
     json (dados) {
         return JSON.stringify(dados)
     }
 
-    serializar (dados) {
-        if (this.contentType === 'application/json') {
-            return this.json(this.filtrar(dados))
+    xml (dados) {
+        let tag = this.tagSingular
+
+        if (Array.isArray(dados)) {
+            tag = this.tagPlural
+            dados = dados.map(dado => {
+                return { [this.tagSingular]: dado }
+            })
         }
+        return jsontoxml({ [tag]: dados })
+    }
+
+    serializar (dados) {
+        dados = this.filtrar(dados)
+
+        if (this.contentType === 'application/json') return this.json(dados)
+        if (this.contentType === 'application/xml') return this.xml(dados)
 
         throw new ValorNaoSuportado(this.contentType)
     }
@@ -37,6 +51,8 @@ class SerializadorFornecedor extends Serializador {
         super()
         this.contentType = contentType
         this.publicFields = ['id', 'empresa', 'categoria'].concat(extraFields || [])
+        this.tagSingular = 'fornecedor'
+        this.tagPlural = 'fornecedores'
     }
 }
 
@@ -45,6 +61,8 @@ class SerializadorError extends Serializador {
         super()
         this.contentType = contentType
         this.publicFields = ['id', 'erro'].concat(extraFields || [])
+        this.tagSingular = 'erro'
+        this.tagPlural = 'erros'
     }
 }
 
@@ -52,5 +70,5 @@ module.exports = {
     Serializador: Serializador,
     SerializadorFornecedor: SerializadorFornecedor,
     SerializadorError: SerializadorError,
-    formatosAceitos: ['application/json']
+    formatosAceitos: ['application/json', 'application/xml']
 }
